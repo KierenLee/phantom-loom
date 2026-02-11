51825053190f777d879e23d820c4c386d72d99d1
KierenLee <909525951@qq.com>
2026-02-11 14:50:44 +0800

    fix: 处理 window 未定义时获取 sessionId 的问题

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getSessionId = () => {
+  if (typeof window === "undefined") {
+    return "";
+  }
   let id = sessionStorage.getItem("thread_id");
   if (!id) {
     id = Math.random().toString(36).substring(7);
